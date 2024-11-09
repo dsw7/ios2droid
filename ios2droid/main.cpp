2f34e5c47f62abeb153bc84ecfd7f3ada6875fc7
dsw7 <dds3443@hotmail.com>
2024-11-09 05:29:10 -0800

    Parse EXIF data using easyexif

--- ios2droid/main.cpp
+++ ios2droid/main.cpp
@@ -1,6 +1,10 @@
 #include <filesystem>
+#include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
+
+#include "exif.h"
 
 void print_build_information()
 {
@@ -8,9 +12,46 @@ void print_build_information()
     std::cout << "Binary compiled on: " << BUILD_DATE << '\n';
 }
 
+bool parse_exif_data(const std::filesystem::path &filepath)
+{
+    std::ifstream file(filepath, std::ios::binary | std::ios::ate);
+
+    if (!file.is_open())
+    {
+        std::cerr << "Failed to open file: " << filepath << '\n';
+        return false;
+    }
+
+    std::streamsize size_file = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    std::vector<unsigned char> buffer;
+    buffer.resize(size_file);
+
+    if (!file.read(reinterpret_cast<char *>(buffer.data()), size_file))
+    {
+        std::cerr << "Failed to read file: " << filepath << '\n';
+        return false;
+    }
+
+    easyexif::EXIFInfo exif_info;
+
+    int rv = exif_info.parseFrom(buffer.data(), buffer.size());
+
+    if (rv != 0)
+    {
+        std::cerr << "Error parsing EXIF: code " << rv << '\n';
+        return false;
+    }
+
+    std::cout << "Date Taken: " << exif_info.DateTimeOriginal << '\n';
+    return true;
+}
+
 void rename_file(const std::filesystem::path &filepath)
 {
     std::cout << filepath << '\n';
+    parse_exif_data(filepath);
 }
 
 void process_directory()
