b5ad075cd58e923fe8e1faa02368898f0db7565f
dsw7 <dds3443@hotmail.com>
2024-11-09 00:20:51 -0800

    Check if dir is empty

--- ios2droid/src/main.cpp
+++ ios2droid/src/main.cpp
@@ -1,4 +1,28 @@
+#include <filesystem>
+#include <iostream>
+#include <string>
+
+void print_build_information()
+{
+    std::cout << "Project version: " << PROJECT_VERSION << '\n';
+    std::cout << "Binary compiled on: " << BUILD_DATE << '\n';
+}
+
+void process_directory()
+{
+    const std::filesystem::path cwd = std::filesystem::current_path();
+    std::cout << "Will rename files in directory: " << cwd << '\n';
+
+    if (std::filesystem::is_empty(cwd))
+    {
+        std::cout << "Directory is empty!\n";
+        return;
+    }
+}
+
 int main()
 {
+    print_build_information();
+    process_directory();
     return 0;
 }
