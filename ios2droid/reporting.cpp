3d67cd4a4e2dc05f4441e2af01db7fc614ff5e6b
dsw7 <dds3443@hotmail.com>
2024-11-10 23:05:51 -0800

    Add a stdout printer

--- ios2droid/reporting.cpp
+++ ios2droid/reporting.cpp
@@ -2,22 +2,55 @@
 
 #include <cstdlib>
 #include <errno.h>
+#include <fmt/core.h>
+#include <iostream>
+#include <optional>
 #include <string.h>
+#include <string>
 
 namespace
 {
-const char *CURRENT_FILE = "CURR_FILE";
+
+const char *TARGET = "CURRENT_FILE";
+
+std::optional<const std::string> get_target()
+{
+    const char *target = std::getenv(TARGET);
+
+    if (target == nullptr)
+    {
+        return std::nullopt;
+    }
+
+    return std::string(target);
 }
+
+} // namespace
 
 namespace reporting
 {
 
 void set_target(const std::string &filename)
 {
-    if (setenv(CURRENT_FILE, filename.c_str(), 1) != 0)
+    if (setenv(TARGET, filename.c_str(), 1) != 0)
     {
-        exit(strerror(errno));
+        std::cerr << strerror(errno) << '\n';
+        exit(EXIT_FAILURE);
     }
+}
+
+void unset_target()
+{
+    if (unsetenv(TARGET) != 0)
+    {
+        std::cerr << strerror(errno) << '\n';
+        exit(EXIT_FAILURE);
+    }
+}
+
+void print_to_stdout(const std::string &message)
+{
+    std::cout << fmt::format("[{}] {}\n", get_target().value_or("Unset"), message);
 }
 
 } // namespace reporting
--- ios2droid/reporting.hpp
+++ ios2droid/reporting.hpp
@@ -6,5 +6,8 @@ namespace reporting
 {
 
 void set_target(const std::string &filename);
+void unset_target();
 
-}
+void print_to_stdout(const std::string &message);
+
+} // namespace reporting
