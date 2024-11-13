a4a8ef1712b27eb66b92beb24591de34aac60b0c
dsw7 <dds3443@hotmail.com>
2024-11-13 02:38:12 -0800

    Add android unit test

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY = help compile clean lint format test
+.PHONY = help compile clean lint format test py
 .DEFAULT_GOAL = help
 
 define HELP_LIST_TARGETS
@@ -12,6 +12,8 @@ To format code:
     $$ make format
 To run unit tests:
     $$ make test
+To lint, analyze and format python code:
+    $$ make py
 endef
 
 export HELP_LIST_TARGETS
@@ -34,3 +36,7 @@ format:
 
 test: compile
 	@python3 -m unittest -v tests/*.py
+
+py:
+	@python3 -m mypy --strict tests
+	@python3 -m black tests
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,15 +1,29 @@
 from os import EX_OK
-from subprocess import run, DEVNULL
+from subprocess import run, DEVNULL, PIPE
 from unittest import TestCase
 
 
 class TestBasic(TestCase):
     command = "build/ios2droid"
 
-    def test_help_short(self):
+    def test_help_short(self) -> None:
         process = run([self.command, "-h"], stdout=DEVNULL, stderr=DEVNULL)
         self.assertEqual(process.returncode, EX_OK)
 
-    def test_help(self):
+    def test_help(self) -> None:
         process = run([self.command, "--help"], stdout=DEVNULL, stderr=DEVNULL)
         self.assertEqual(process.returncode, EX_OK)
+
+    def test_inspect_android(self) -> None:
+        process = run([self.command, "tests/jpg_android.jpg"], stdout=PIPE, stderr=PIPE)
+        self.assertEqual(process.returncode, EX_OK)
+
+        make: str | None = None
+        for line in process.stdout.decode().splitlines():
+            if "Camera make" in line:
+                make = line.split(": ")[-1]
+                break
+        else:
+            self.fail("Could not find 'Camera make' line in stdout")
+
+        self.assertEqual(make, "LGE")
