6b63f52be6591a8c67d49da0cafedf0ba7746097
dsw7 <dds3443@hotmail.com>
2024-11-13 03:02:32 -0800

    Add Apple test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -14,16 +14,24 @@ class TestBasic(TestCase):
         process = run([self.command, "--help"], stdout=DEVNULL, stderr=DEVNULL)
         self.assertEqual(process.returncode, EX_OK)
 
-    def test_inspect_android(self) -> None:
-        process = run([self.command, "tests/jpg_android.jpg"], stdout=PIPE, stderr=PIPE)
-        self.assertEqual(process.returncode, EX_OK)
+    def test_inspect_jpgs(self) -> None:
+        test_cases = [
+            ("tests/jpg_android.jpg", "LGE"),
+            ("tests/jpg_apple.jpg", "Apple"),
+        ]
 
-        make: str | None = None
-        for line in process.stdout.decode().splitlines():
-            if "Camera make" in line:
-                make = line.split(": ")[-1]
-                break
-        else:
-            self.fail("Could not find 'Camera make' line in stdout")
+        for filename, exp_make in test_cases:
+            with self.subTest(filename=filename, exp_make=exp_make):
+                process = run([self.command, filename], stdout=PIPE, stderr=PIPE)
 
-        self.assertEqual(make, "LGE")
+                self.assertEqual(process.returncode, EX_OK)
+                make: str | None = None
+
+                for line in process.stdout.decode().splitlines():
+                    if "Camera make" in line:
+                        make = line.split(": ")[-1]
+                        break
+                else:
+                    self.fail("Could not find 'Camera make' line in stdout")
+
+                self.assertEqual(make, exp_make)
