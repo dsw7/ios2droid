2f0135f5252daca626e734e4efa4e9cb7504433c
dsw7 <dds3443@hotmail.com>
2024-11-14 00:04:10 -0800

    Add missing file test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,17 +2,21 @@ from os import EX_OK
 from subprocess import run, DEVNULL, PIPE
 from unittest import TestCase
 
+PATH_BIN = "build/ios2droid"
 
-class TestBasic(TestCase):
-    command = "build/ios2droid"
+
+class TestHelpMessages(TestCase):
 
     def test_help_short(self) -> None:
-        process = run([self.command, "-h"], stdout=DEVNULL, stderr=DEVNULL)
+        process = run([PATH_BIN, "-h"], stdout=DEVNULL, stderr=DEVNULL)
         self.assertEqual(process.returncode, EX_OK)
 
     def test_help(self) -> None:
-        process = run([self.command, "--help"], stdout=DEVNULL, stderr=DEVNULL)
+        process = run([PATH_BIN, "--help"], stdout=DEVNULL, stderr=DEVNULL)
         self.assertEqual(process.returncode, EX_OK)
+
+
+class TestInspectFile(TestCase):
 
     def test_inspect_jpgs(self) -> None:
         test_cases = [
@@ -22,7 +26,7 @@ class TestBasic(TestCase):
 
         for filename, exp_make in test_cases:
             with self.subTest(filename=filename, exp_make=exp_make):
-                process = run([self.command, filename], stdout=PIPE, stderr=PIPE)
+                process = run([PATH_BIN, filename], stdout=PIPE, stderr=DEVNULL)
 
                 self.assertEqual(process.returncode, EX_OK)
                 make: str | None = None
@@ -35,3 +39,14 @@ class TestBasic(TestCase):
                     self.fail("Could not find 'Camera make' line in stdout")
 
                 self.assertEqual(make, exp_make)
+
+    def test_missing_file(self) -> None:
+        process = run(
+            [PATH_BIN, "tests/a_missing_file.jpg"], stdout=DEVNULL, stderr=PIPE
+        )
+
+        self.assertNotEqual(process.returncode, EX_OK)
+        self.assertEqual(
+            process.stderr.decode().strip(),
+            "File 'tests/a_missing_file.jpg' does not exist",
+        )
