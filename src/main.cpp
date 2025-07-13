026d5cac25b01b45695da9fd8ff742e201aa02f6
dsw7 <dds3443@hotmail.com>
2025-07-13 02:58:50 -0700

    Reduce scope of help messages + tweak formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include "help.hpp"
 #include "inspect.hpp"
 #include "rename_files.hpp"
 
@@ -6,6 +5,41 @@
 #include <optional>
 #include <stdexcept>
 #include <string>
+
+void print_help_messages()
+{
+    std::cout << "-- iOS2Droid | v" << PROJECT_VERSION << '\n';
+    std::cout << "-- Site: https://github.com/dsw7/ios2droid\n";
+
+    const std::string messages = R"(
+Rename iOS originating photos:
+  IMG_XXXX.JPG -> YYYYMMDD_HHMMSS.jpg
+
+Program also offers a means of inspecting the EXIF metadata
+associated with a photo.
+
+Usage:
+  ios2droid [OPTION]
+  ios2droid FILE
+
+Options:
+  -h, --help    Show this help message and exit
+  -r, --rename  Rename files of iOS origin
+
+Arguments:
+  FILE          The file to inspect (optional)
+
+Examples:
+  1. Run a dry run. Will not actually rename files:
+    $ ios2droid (no arguments)
+  2. Rename files in current working directory if satisfied with dry run:
+    $ ios2droid -r
+  3. To inspect EXIF metadata associated with a file:
+    $ ios2droid FILE
+)";
+
+    std::cout << messages << '\n';
+}
 
 void process_input(const std::optional<std::string> &option)
 {
@@ -17,9 +51,9 @@ void process_input(const std::optional<std::string> &option)
     const std::string file_or_opt = option.value();
 
     if (file_or_opt == "--help") {
-        print_summary();
+        print_help_messages();
     } else if (file_or_opt == "-h") {
-        print_summary();
+        print_help_messages();
     } else if (file_or_opt == "--rename") {
         rename_files(false);
     } else if (file_or_opt == "-r") {
